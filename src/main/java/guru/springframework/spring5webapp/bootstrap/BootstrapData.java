f01202917de5b674d5c1ed9363aabd76491413c5
Eduardo Both <eduardoboth@gmail.com>
2020-07-26 00:17:14 -0300

    Adds Publisher-Book relationship; enables hs console.

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -24,6 +24,8 @@ public class BootstrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        Publisher bestPublisher = new Publisher("Best Publisher", "Somewhere far.");
+        publisherRepository.save(bestPublisher);
 
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "133213");
@@ -31,8 +33,12 @@ public class BootstrapData implements CommandLineRunner {
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
+        ddd.setPublisher(bestPublisher);
+        bestPublisher.getBooks().add(ddd);
+
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(bestPublisher);
 
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development without EJB", "654123");
@@ -40,16 +46,17 @@ public class BootstrapData implements CommandLineRunner {
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
+        noEJB.setPublisher(bestPublisher);
+        bestPublisher.getBooks().add(noEJB);
+
         authorRepository.save(rod);
         bookRepository.save(noEJB);
-
-        Publisher bestPublisher = new Publisher("Best Publisher", "Somewhere far.");
-
         publisherRepository.save(bestPublisher);
 
         System.out.println("Started in bootstrap");
         System.out.println("Number of books: " + bookRepository.count());
         System.out.println("Number of authors: " + authorRepository.count());
         System.out.println("Number of publishers: " + publisherRepository.count());
+        System.out.println("Books for Best Publisher: " + bestPublisher.getBooks().size());
     }
 }
--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -1,9 +1,8 @@
 package guru.springframework.spring5webapp.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Publisher {
@@ -12,14 +11,18 @@ public class Publisher {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String name;
-    private String address;
+    private String addressLineOne;
+
+    @OneToMany
+    @JoinColumn(name = "publisher_id")
+    private Set<Book> books = new HashSet<>();
 
     public Publisher() {
     }
 
-    public Publisher(String name, String address) {
+    public Publisher(String name, String addressLineOne) {
         this.name = name;
-        this.address = address;
+        this.addressLineOne = addressLineOne;
     }
 
     public long getId() {
@@ -38,12 +41,20 @@ public class Publisher {
         this.name = name;
     }
 
-    public String getAddress() {
-        return address;
+    public String getAddressLineOne() {
+        return addressLineOne;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
+    public void setAddressLineOne(String address) {
+        this.addressLineOne = address;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
     }
 
     @Override
