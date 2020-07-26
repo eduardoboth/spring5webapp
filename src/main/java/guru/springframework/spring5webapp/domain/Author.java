d7cf5ba2df5d09cc7eaa6cafcf5806a420d20aa4
Eduardo Both <eduardoboth@gmail.com>
2020-07-26 01:02:11 -0300

    Implements Book Controller with list all

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -74,8 +74,7 @@ public class Author {
         return "Author{" +
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", books=" + books +
+                ", lastName='" + lastName +
                 '}';
     }
 }
